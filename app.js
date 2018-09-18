0a30d0880e8d609e937c56e11079ca4e2a94486f
Mike Choi <mike.choi@live.ca>
2018-09-17 22:56:24 -0700

    Added local storage functionality to the ES5 version

--- app.js
+++ app.js
@@ -6,18 +6,16 @@ function Book(title, author, isbn) {
 }
 
 // UI Constructor
-function UI() {}
-// Instantiate UI
-const ui = new UI();
+function UI(){}
 
 UI.prototype.addBookToList = function(book){
     const list = document.querySelector('#book-list');
     const row = document.createElement('tr');
     row.innerHTML = `
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="delete">X</a></td>
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
     `;
     list.appendChild(row);
 }
@@ -35,14 +33,14 @@ UI.prototype.showAlert = function(message, className){
     div.className = `alert ${className}`;
     // Add text
     div.appendChild(document.createTextNode(message));
-
+    
     // Get parent and form
     const container = document.querySelector('.container');
     const form = document.querySelector('#book-form');
-
+    
     // Insert div
     container.insertBefore(div, form);
-
+    
     // Timeout after 3 seconds
     setTimeout(function(){
         document.querySelector('.alert').remove();
@@ -56,7 +54,57 @@ UI.prototype.deleteBook = function(target){
     }
 }
 
+// Store
+function Store(){
+    getBooks = function(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            return books = [];
+        } else{
+            return books = JSON.parse(localStorage.getItem('books'));
+        }        
+    }
+
+    displayBooks = function(){
+        const books = this.getBooks();
+    
+        books.forEach(function(book){
+            ui.addBookToList(book);
+        });
+    }
+}
+
+Store.addBook = function(book){
+    if(book !== null){
+        const books = this.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+Store.removeBook = function(target){
+    if(target.className === 'delete'){
+        const title     = target.parentElement.parentElement.children[0].textContent;
+        const author    = target.parentElement.parentElement.children[1].textContent;
+        const isbn      = target.parentElement.parentElement.children[2].textContent;
+        
+        const books = this.getBooks();
+        books.forEach(function(book, index){
+            if(book.title === title && book.author === author && book.isbn === isbn){
+                books.splice(index,1);
+            }
+        });
+    
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// Instantiate UI
+const ui = new UI();
+
 // Event Listeners
+document.addEventListener('DOMContentLoaded', Store.displayBooks());
+
 document.querySelector('#book-form').addEventListener('submit', function(e){
     e.preventDefault();
     
@@ -77,6 +125,8 @@ document.querySelector('#book-form').addEventListener('submit', function(e){
         ui.showAlert('Book added!', 'success');
         // Add book to list
         ui.addBookToList(book);
+        // Add book to LS
+        Store.addBook(book);
         // Clear fields
         ui.clearFields();
     }    
@@ -84,5 +134,6 @@ document.querySelector('#book-form').addEventListener('submit', function(e){
 
 document.querySelector('#book-list').addEventListener('click', function(e){
     ui.deleteBook(e.target);
+    Store.removeBook(e.target);
     e.preventDefault();
 });
